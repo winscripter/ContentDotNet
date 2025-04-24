859c1a95b3101dc3b122e2ebe4059bee44125a4c
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-04-24 22:51:20 +0400

    Add equality, gethashcode and tostring

--- src/ContentDotNet.Abstractions/PerformanceMetrics.cs
+++ src/ContentDotNet.Abstractions/PerformanceMetrics.cs
@@ -5,7 +5,7 @@ namespace ContentDotNet.Abstractions;
 /// <summary>
 /// Performance Metrics control performance options for the encoder/decoder.
 /// </summary>
-public sealed class PerformanceMetrics : INotifyPropertyChanged
+public sealed class PerformanceMetrics : INotifyPropertyChanged, IEquatable<PerformanceMetrics?>
 {
     /// <summary>
     ///   Invoked when a property is changed.
@@ -31,7 +31,8 @@ public sealed class PerformanceMetrics : INotifyPropertyChanged
     ///   Initializes a new instance of the <see cref="PerformanceMetrics"/> class with default values,
     ///   which include disabling multithreading and not using any accelerators.
     /// </summary>
-    public PerformanceMetrics() : this(false, 1, false, null)
+    public PerformanceMetrics()
+        : this(false, 1, false, null)
     {
     }
 
@@ -136,8 +137,91 @@ public sealed class PerformanceMetrics : INotifyPropertyChanged
         }
     }
 
+    /// <summary>
+    ///   Raises the <see cref="PropertyChanged"/> event for the specified property.
+    /// </summary>
+    /// <param name="propertyName">The name of the property that changed.</param>
     private void RaisePropertyChanged(string propertyName)
     {
         this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    /// <summary>
+    ///   Determines whether the specified object is equal to the current <see cref="PerformanceMetrics"/> instance.
+    /// </summary>
+    /// <param name="obj">The object to compare with the current instance.</param>
+    /// <returns>
+    ///   <c>true</c> if the specified object is equal to the current instance; otherwise, <c>false</c>.
+    /// </returns>
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as PerformanceMetrics);
+    }
+
+    /// <summary>
+    ///   Determines whether the specified <see cref="PerformanceMetrics"/> instance is equal to the current instance.
+    /// </summary>
+    /// <param name="other">The <see cref="PerformanceMetrics"/> instance to compare with the current instance.</param>
+    /// <returns>
+    ///   <c>true</c> if the specified instance is equal to the current instance; otherwise, <c>false</c>.
+    /// </returns>
+    public bool Equals(PerformanceMetrics? other)
+    {
+        return other is not null &&
+               _isMultithreaded == other._isMultithreaded &&
+               IsMultithreaded == other.IsMultithreaded &&
+               _threadCount == other._threadCount &&
+               ThreadCount == other.ThreadCount &&
+               ActualThreadCount == other.ActualThreadCount &&
+               _enableHardwareAcceleration == other._enableHardwareAcceleration &&
+               EnableHardwareAcceleration == other.EnableHardwareAcceleration &&
+               _hardwareAcceleratorName == other._hardwareAcceleratorName &&
+               HardwareAcceleratorName == other.HardwareAcceleratorName;
+    }
+
+    /// <summary>
+    ///   Serves as the default hash function.
+    /// </summary>
+    /// <returns>A hash code for the current <see cref="PerformanceMetrics"/> instance.</returns>
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+        hash.Add(_isMultithreaded);
+        hash.Add(IsMultithreaded);
+        hash.Add(_threadCount);
+        hash.Add(ThreadCount);
+        hash.Add(ActualThreadCount);
+        hash.Add(_enableHardwareAcceleration);
+        hash.Add(EnableHardwareAcceleration);
+        hash.Add(_hardwareAcceleratorName);
+        hash.Add(HardwareAcceleratorName);
+        return hash.ToHashCode();
+    }
+
+
+    /// <summary>
+    ///   Determines whether two <see cref="PerformanceMetrics"/> instances are equal.
+    /// </summary>
+    /// <param name="left">The first <see cref="PerformanceMetrics"/> instance to compare.</param>
+    /// <param name="right">The second <see cref="PerformanceMetrics"/> instance to compare.</param>
+    /// <returns>
+    ///   <c>true</c> if the two instances are equal; otherwise, <c>false</c>.
+    /// </returns>
+    public static bool operator ==(PerformanceMetrics? left, PerformanceMetrics? right)
+    {
+        return EqualityComparer<PerformanceMetrics>.Default.Equals(left, right);
+    }
+
+    /// <summary>
+    ///   Determines whether two <see cref="PerformanceMetrics"/> instances are not equal.
+    /// </summary>
+    /// <param name="left">The first <see cref="PerformanceMetrics"/> instance to compare.</param>
+    /// <param name="right">The second <see cref="PerformanceMetrics"/> instance to compare.</param>
+    /// <returns>
+    ///   <c>true</c> if the two instances are not equal; otherwise, <c>false</c>.
+    /// </returns>
+    public static bool operator !=(PerformanceMetrics? left, PerformanceMetrics? right)
+    {
+        return !(left == right);
     }
 }
