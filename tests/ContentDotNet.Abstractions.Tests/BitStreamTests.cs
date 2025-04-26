e35ffd1127de33ab7778d06ba159093e3faf7a6a
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-04-26 21:04:48 +0400

    First unit test!

---
+++ tests/ContentDotNet.Abstractions.Tests/BitStreamTests.cs
@@ -0,0 +1,30 @@
+﻿namespace ContentDotNet.Abstractions.Tests;
+
+public class BitStreamTests
+{
+    [Fact]
+    public void UE_Golomb_1()
+    {
+        UseBSWriterThenReader(
+            (writer) =>
+            {
+                writer.WriteUE(1);
+            },
+            (reader) =>
+            {
+                uint ueGolomb = reader.ReadUE();
+                Assert.Equal(1u, ueGolomb);
+            });
+    }
+
+    private static void UseBSWriterThenReader(Action<BitStreamWriter> writer, Action<BitStreamReader> reader)
+    {
+        using var msWriter = new MemoryStream();
+        using var bw = new BitStreamWriter(msWriter);
+        writer(bw);
+
+        using var msReader = new MemoryStream(msWriter.ToArray());
+        using var br = new BitStreamReader(msReader);
+        reader(br);
+    }
+}
