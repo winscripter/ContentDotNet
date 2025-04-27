3b024c1af18bcf7d5caa3c31983bc67d04d15f26
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-04-27 17:03:12 +0400

    Fix ue(v) tests

--- tests/ContentDotNet.Abstractions.Tests/BitStreamTests.cs
+++ tests/ContentDotNet.Abstractions.Tests/BitStreamTests.cs
@@ -273,7 +273,7 @@ public class BitStreamTests
             reader =>
             {
                 uint b = reader.ReadUE();
-                Assert.Equal(VALUE + 1, b);
+                Assert.Equal(VALUE - 1, b);
             });
     }
 
