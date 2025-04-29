0ba3f6fd945cc41ec78deadac97e23a986e2f215
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-04-29 19:12:16 +0400

    Test NAL start codes (Not sure if this one will work though)

--- tests/ContentDotNet.Extensions.H264.Tests/ModelsIO/NALTests.cs
+++ tests/ContentDotNet.Extensions.H264.Tests/ModelsIO/NALTests.cs
@@ -1,109 +1,38 @@
-﻿//using ContentDotNet.Abstractions;
-//using ContentDotNet.Extensions.H264.Models;
+﻿using ContentDotNet.Abstractions;
+using ContentDotNet.Extensions.H264.Models;
 
-//namespace ContentDotNet.Extensions.H264.Tests.ModelsIO;
+namespace ContentDotNet.Extensions.H264.Tests.ModelsIO;
 
-//public class NALTests
-//{
-//    [Fact]
-//    public void Nalu_Start_Code_Immediate()
-//    {
-//        var ms = new MemoryStream();
-//        var bw = new BitStreamWriter(ms);
+public class NALTests
+{
+    [Fact]
+    public void TestStartCodes()
+    {
+        UseBSWriterThenReader(
+            writer =>
+            {
+                for (int i = 0; i < 6; i++)
+                    writer.WriteBits(0, 8);
+                writer.WriteBits(1u, 8);
 
-//        bw.WriteBits(0u, 8);
-//        Assert.Equal(0, bw.BitPosition);
-//        bw.WriteBits(0u, 8);
-//        Assert.Equal(0, bw.BitPosition);
-//        bw.WriteBits(0u, 8);
-//        Assert.Equal(0, bw.BitPosition);
-//        bw.WriteBits(1u, 8);
-//        Assert.Equal(0, bw.BitPosition);
+                // filler data
+                writer.WriteBits(0, 16);
+            },
+            reader =>
+            {
+                Assert.True(NalUnit.SkipStartCode(reader));
+                Assert.Equal(6, reader.BaseStream.Position);
+            });
+    }
 
-//        var nalu = new NalUnit(1, 6, false, false, null);
-//        nalu.Write(bw);
+    private static void UseBSWriterThenReader(Action<BitStreamWriter> writer, Action<BitStreamReader> reader)
+    {
+        using var msWriter = new MemoryStream();
+        using var bw = new BitStreamWriter(msWriter);
+        writer(bw);
 
-//        bw.WriteBits(0, 32);
-//        bw.WriteBits(0, 32);
-
-//        // -----------
-
-//        ms.Position = 0;
-//        var br = new BitStreamReader(ms);
-
-//        NalUnit.SkipStartCode(br);
-//        _ = br.ReadByte();
-
-//        Assert.Equal(5, br.BaseStream.Position);
-//        Assert.Equal(1, br.GetState().BitPosition);
-
-//        NalUnit newNalu = NalUnit.Read(br, 1);
-
-//        Assert.Equal(nalu.NalRefIdc, newNalu.NalRefIdc);
-//        Assert.Equal(nalu.NalUnitType, newNalu.NalUnitType);
-//        Assert.Equal(nalu.SvcExtensionFlag, newNalu.SvcExtensionFlag);
-//        Assert.Equal(nalu.Avc3DExtensionFlag, newNalu.Avc3DExtensionFlag);
-//        Assert.Equal(nalu.Extension, newNalu.Extension);
-
-//        try
-//        {
-//            br.Dispose();
-//            bw.Dispose();
-//            ms.Dispose();
-//        }
-//        catch
-//        {
-//        }
-//    }
-
-//    [Fact]
-//    public void Nalu_Start_Code_Immediate_2()
-//    {
-//        var ms = new MemoryStream();
-//        var bw = new BitStreamWriter(ms);
-
-//        bw.WriteBits(0u, 8);
-//        bw.WriteBits(0u, 8);
-//        bw.WriteBits(0u, 8);
-//        bw.WriteBits(0u, 8);
-//        bw.WriteBits(0u, 8);
-//        bw.WriteBits(0u, 8);
-//        bw.WriteBits(1u, 8);
-
-//        var nalu = new NalUnit(1, 6, false, false, null);
-//        nalu.Write(bw);
-
-//        bw.WriteBits(0, 32);
-//        bw.WriteBits(0, 32);
-
-//        // -----------
-
-//        ms.Position = 0;
-//        var br = new BitStreamReader(ms);
-
-//        NalUnit.SkipStartCode(br);
-//        Assert.Equal(1, br.GetState().BitPosition);
-//        _ = br.ReadBits(8);
-
-//        Assert.Equal(8, br.BaseStream.Position);
-//        Assert.Equal(1, br.GetState().BitPosition);
-
-//        NalUnit newNalu = NalUnit.Read(br, 1);
-
-//        Assert.Equal(nalu.NalRefIdc, newNalu.NalRefIdc);
-//        Assert.Equal(nalu.NalUnitType, newNalu.NalUnitType);
-//        Assert.Equal(nalu.SvcExtensionFlag, newNalu.SvcExtensionFlag);
-//        Assert.Equal(nalu.Avc3DExtensionFlag, newNalu.Avc3DExtensionFlag);
-//        Assert.Equal(nalu.Extension, newNalu.Extension);
-
-//        try
-//        {
-//            br.Dispose();
-//            bw.Dispose();
-//            ms.Dispose();
-//        }
-//        catch
-//        {
-//        }
-//    }
-//}
+        using var msReader = new MemoryStream(msWriter.ToArray());
+        using var br = new BitStreamReader(msReader);
+        reader(br);
+    }
+}
