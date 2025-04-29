2b071850a88675be37840d5a49964b232bcac254
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-04-29 15:25:03 +0400

    NALU test 2

--- tests/ContentDotNet.Extensions.H264.Tests/ModelsIO/NALTests.cs
+++ tests/ContentDotNet.Extensions.H264.Tests/ModelsIO/NALTests.cs
@@ -40,4 +40,43 @@ public class NALTests
         {
         }
     }
+
+    [Fact]
+    public void Nalu_Start_Code_Immediate_2()
+    {
+        var ms = new MemoryStream();
+        var bw = new BitStreamWriter(ms);
+
+        bw.WriteBits(0u, 8);
+        bw.WriteBits(0u, 8);
+        bw.WriteBits(0u, 8);
+        bw.WriteBits(0u, 8);
+        bw.WriteBits(0u, 8);
+        bw.WriteBits(0u, 8);
+        bw.WriteBits(1u, 8);
+
+        var nalu = new NalUnit(5, 6, false, false, null);
+        nalu.Write(bw);
+
+        bw.WriteBits(0, 10);
+
+        // -----------
+
+        ms.Position = 0;
+        var br = new BitStreamReader(ms);
+
+        NalUnit.SkipStartCode(br);
+
+        Assert.Equal(8, br.BaseStream.Position);
+
+        try
+        {
+            br.Dispose();
+            bw.Dispose();
+            ms.Dispose();
+        }
+        catch
+        {
+        }
+    }
 }
