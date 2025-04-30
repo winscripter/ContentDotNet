30a16744861cdfb37e2d1f975d4c269dc987036a
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-04-30 14:12:33 +0400

    Another abstraction

---
+++ src/ContentDotNet.Extensions.H264/H264Extension.cs
@@ -0,0 +1,27 @@
+﻿namespace ContentDotNet.Extensions.H264;
+
+/// <summary>
+///   Represents the H.264 extension to use.
+/// </summary>
+public enum H264Extension
+{
+    /// <summary>
+    ///   Extensions are not used.
+    /// </summary>
+    Baseline = 0,
+
+    /// <summary>
+    ///   Use Scalable View Coding (SVC).
+    /// </summary>
+    ScalableVideoCoding = 1,
+
+    /// <summary>
+    ///   Use Multiview Video Coding (MVC).
+    /// </summary>
+    MultiviewVideoCoding = 2,
+
+    /// <summary>
+    ///   3D AVC, aka Multiview and Depth Video with enhanced non-base Coding.
+    /// </summary>
+    Avc3D = 3,
+}
