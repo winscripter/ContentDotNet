47d37045a8d7dbb87ffc7d7d9762870dfeed2a1d
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-05-06 12:30:56 +0400

    CABAC ctxIdx

---
+++ src/ContentDotNet.Extensions.H264/CabacContext.cs
@@ -0,0 +1,17 @@
+﻿namespace ContentDotNet.Extensions.H264;
+
+/// <summary>
+///   Represents context for CABAC bitstream parsing.
+/// </summary>
+public struct CabacContext
+{
+    /// <summary>
+    ///   PStateIdx
+    /// </summary>
+    public uint PStateIdx;
+
+    /// <summary>
+    ///   ValMps
+    /// </summary>
+    public bool ValMps;
+}
