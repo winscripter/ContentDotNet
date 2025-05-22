dd4bdd447e9fb15256d6a15006fa7aa14670e926
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-05-22 21:34:42 +0400

    Add abstractions

---
+++ src/ContentDotNet.Extensions.H264/H264MacroblockMode.cs
@@ -0,0 +1,27 @@
+﻿namespace ContentDotNet.Extensions.H264;
+
+/// <summary>
+///   Represents the macroblock mode for H.264.
+/// </summary>
+public enum H264MacroblockMode
+{
+    /// <summary>
+    ///   A PCM macroblock (raw data).
+    /// </summary>
+    Pcm,
+
+    /// <summary>
+    ///   An Intra macroblock (I-frame).
+    /// </summary>
+    Intra,
+
+    /// <summary>
+    ///   An Inter macroblock (P- and B-frame).
+    /// </summary>
+    Inter,
+
+    /// <summary>
+    ///   The macroblock mode is derived automatically.
+    /// </summary>
+    Auto
+}
