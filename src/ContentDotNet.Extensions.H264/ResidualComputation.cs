29e895cccb29036c2a79c64f086c5931afc263ba
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-05-26 15:43:01 +0400

    Add ResidualComputation and blank PNG library

---
+++ src/ContentDotNet.Extensions.H264/ResidualComputation.cs
@@ -0,0 +1,32 @@
+﻿using ContentDotNet.Extensions.H264.Containers;
+using ContentDotNet.Extensions.H264.Models;
+
+namespace ContentDotNet.Extensions.H264;
+
+/// <summary>
+///   Computes H.264 residuals.
+/// </summary>
+public static class ResidualComputation
+{
+    /// <summary>
+    ///   Computes the residual between two 16x16 matrices.
+    /// </summary>
+    /// <param name="predicted">Predicted value</param>
+    /// <param name="expected">Expected value</param>
+    /// <returns>A residual</returns>
+    public static ContainerMatrix16x16 Differentiate(ContainerMatrix16x16 predicted, ContainerMatrix16x16 expected)
+    {
+        ContainerMatrix16x16 residual = default;
+
+        for (int x = 0; x < 16; x++)
+        {
+            for (int y = 0; y < 16; y++)
+            {
+                // Negative values are allowed
+                residual[x, y] = expected[x, y] - predicted[x, y];
+            }
+        }
+
+        return residual;
+    }
+}
