51b68c7a81d1e45b876545101d85d949c8d1a922
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-06-07 21:07:30 +0400

    Add weighted prediction

--- src/ContentDotNet.Extensions.H264/Internal/Encoding/H264MacroblockPredictor.cs
+++ src/ContentDotNet.Extensions.H264/Internal/Encoding/H264MacroblockPredictor.cs
@@ -1,6 +1,10 @@
-﻿using ContentDotNet.Extensions.H264.Internal.Encoding.Predicted;
+﻿using ContentDotNet.Abstractions;
+using ContentDotNet.Containers;
+using ContentDotNet.Extensions.H264.Internal.Encoding.Predicted;
+using ContentDotNet.Extensions.H264.Models;
 using ContentDotNet.Extensions.H264.Pictures;
 using ContentDotNet.Extensions.H264.Utilities;
+using ContentDotNet.Primitives;
 using System.Drawing;
 
 namespace ContentDotNet.Extensions.H264.Internal.Encoding;
@@ -72,5 +76,83 @@ internal static class H264MacroblockPredictor
 
             return null;
         }
+    }
+
+    // I used ChatGPT for this 💀🙏🏻 wish me luck
+    // if only the spec provided how to actually encode H.264
+    public static WeightedPredictionResult? AnalyzeWeightedPrediction(
+        Dpb dpbL0,
+        int dpbSizeL0,
+        Matrix16x16 srcL,
+        Size partitionSize, bool isBSlice)
+    {
+        const uint defaultDenom = 5;
+        var lumaWeightL0 = new Container32Int32();
+        var lumaOffsetL0 = new Container32Int32();
+        var chromaWeightL0 = new ContainerMatrix2x32();
+        var chromaOffsetL0 = new ContainerMatrix2x32();
+        var lumaWeightL0Flag = new Container32Boolean();
+        var chromaWeightL0Flag = new Container32Boolean();
+
+        bool anyWeightingUsed = false;
+
+        for (int i = 0; i < dpbSizeL0; i++)
+        {
+            var refPic = dpbL0[i];
+            if (refPic is null) continue;
+
+            int weightSum = 0, offsetSum = 0, pixelCount = 0;
+
+            for (int y = 0; y < partitionSize.Height; y++)
+            {
+                for (int x = 0; x < partitionSize.Width; x++)
+                {
+                    int refVal = refPic.Frame.Y[x, y];
+                    int srcVal = srcL[x, y];
+
+                    int offset = srcVal - refVal;
+                    offsetSum += offset;
+                    weightSum += (srcVal * refVal);
+                    pixelCount++;
+                }
+            }
+
+            if (pixelCount == 0) continue;
+
+            int avgOffset = offsetSum / pixelCount;
+            int avgRef = Math.Max(1, weightSum / pixelCount);
+
+            int weight = (srcL[0, 0] << (int)defaultDenom) / avgRef;
+
+            if (weight != (1 << (int)defaultDenom) || avgOffset != 0)
+            {
+                lumaWeightL0Flag[i] = true;
+                lumaWeightL0[i] = weight;
+                lumaOffsetL0[i] = avgOffset;
+                anyWeightingUsed = true;
+            }
+        }
+
+        var table = new PredWeightTable(
+            lumaLog2WeightDenom: defaultDenom,
+            chromaLog2WeightDenom: defaultDenom,
+            lumaWeightL0Flag,
+            lumaWeightL0,
+            lumaOffsetL0,
+            chromaWeightL0Flag,
+            chromaWeightL0,
+            chromaOffsetL0,
+            default, default, default,
+            default, default, default
+        );
+
+        if (!anyWeightingUsed)
+            return null;
+
+        return new WeightedPredictionResult(
+            table,
+            predFlag: !isBSlice,
+            bipredIdc: (byte)(isBSlice ? 1 : 0)
+        );
     }
 }
---
+++ src/ContentDotNet.Extensions.H264/Internal/Encoding/Predicted/WeightedPredictionResult.cs
@@ -0,0 +1,17 @@
+﻿using ContentDotNet.Extensions.H264.Models;
+
+namespace ContentDotNet.Extensions.H264.Internal.Encoding.Predicted;
+
+internal struct WeightedPredictionResult
+{
+    public PredWeightTable WeightTable;
+    public bool WeightedPredFlag;         // 0: off, 1: on (P-slices)
+    public byte WeightedBiPredIdc;        // 0: off, 1: explicit, 2: implicit (B-slices)
+
+    public WeightedPredictionResult(PredWeightTable table, bool predFlag, byte bipredIdc)
+    {
+        WeightTable = table;
+        WeightedPredFlag = predFlag;
+        WeightedBiPredIdc = bipredIdc;
+    }
+}
