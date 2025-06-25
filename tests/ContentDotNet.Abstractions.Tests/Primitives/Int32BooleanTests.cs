6247f437bcdf69bcf7649262a09c65ed8c5590b2
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-06-25 21:38:06 +0400

    Add an Int32Boolean test

---
+++ tests/ContentDotNet.Abstractions.Tests/Primitives/Int32BooleanTests.cs
@@ -0,0 +1,17 @@
+﻿using ContentDotNet.Primitives;
+
+namespace ContentDotNet.Abstractions.Tests.Primitives;
+
+public class Int32BooleanTests
+{
+    [Fact]
+    public void TestFlippedBoolean()
+    {
+        bool source = false;
+        source = Int32Boolean.B(1 - Int32Boolean.I32(source));
+        Assert.True(source);
+
+        source = Int32Boolean.B(1 - Int32Boolean.I32(source));
+        Assert.False(source);
+    }
+}
