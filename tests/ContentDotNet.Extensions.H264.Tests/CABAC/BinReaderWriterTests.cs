5d96abd239a6dfa280050bf57eef28959533c02c
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-07-02 00:45:20 +0400

    Temporarily throw to inspect the size of the generated stream

--- tests/ContentDotNet.Extensions.H264.Tests/CABAC/BinReaderWriterTests.cs
+++ tests/ContentDotNet.Extensions.H264.Tests/CABAC/BinReaderWriterTests.cs
@@ -85,6 +85,8 @@ public class BinReaderWriterTests
                     writer.WriteBin(ref cabac, i % 2 == 0);
             });
 
+        throw new InvalidOperationException("... " + bsr.Length);
+
         var cabac = new CabacContext(0, 0, false, false, 0);
         var dec = new ArithmeticDecoder(bsr);
         for (int i = 0; i < 10000; i++)
