58dea8a3b49bfcd10a49d80e3d30e280a19e93c0
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-07-02 18:23:10 +0400

    Fix tests

--- tests/ContentDotNet.Extensions.H264.Tests/CABAC/InitialContextValueTests.cs
+++ tests/ContentDotNet.Extensions.H264.Tests/CABAC/InitialContextValueTests.cs
@@ -8,15 +8,15 @@ public class InitialContextValueTests
     public void Test_CtxIdx0_CII0_Bypass0_I0_QP0()
     {
         var cabac = new CabacContext(0, 0, false, false, 0);
-        Assert.Equal(0, cabac.PStateIdx);
-        Assert.True(cabac.ValMps);
+        Assert.Equal(62, cabac.PStateIdx);
+        Assert.False(cabac.ValMps);
     }
 
     [Fact]
     public void Test_CtxIdx15_CII0_Bypass0_I0_QP26()
     {
         var cabac = new CabacContext(15, 0, false, false, 26);
-        Assert.Equal(9, cabac.PStateIdx);
+        Assert.Equal(14, cabac.PStateIdx);
         Assert.False(cabac.ValMps);
     }
 
@@ -24,7 +24,7 @@ public class InitialContextValueTests
     public void Test_CtxIdx280_CII0_Bypass0_I0_QP46()
     {
         var cabac = new CabacContext(280, 0, false, false, 46);
-        Assert.Equal(16, cabac.PStateIdx);
-        Assert.False(cabac.ValMps);
+        Assert.Equal(62, cabac.PStateIdx);
+        Assert.True(cabac.ValMps);
     }
 }
