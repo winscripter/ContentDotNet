7b5d2fd62af549af86019d4f70b483a432daad76
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-07-02 18:16:27 +0400

    New unit tests!

---
+++ tests/ContentDotNet.Extensions.H264.Tests/CABAC/InitialContextValueTests.cs
@@ -0,0 +1,30 @@
+﻿using ContentDotNet.Extensions.H264.Cabac;
+
+namespace ContentDotNet.Extensions.H264.Tests.CABAC;
+
+public class InitialContextValueTests
+{
+    [Fact]
+    public void Test_CtxIdx0_CII0_Bypass0_I0_QP0()
+    {
+        var cabac = new CabacContext(0, 0, false, false, 0);
+        Assert.Equal(0, cabac.PStateIdx);
+        Assert.True(cabac.ValMps);
+    }
+
+    [Fact]
+    public void Test_CtxIdx15_CII0_Bypass0_I0_QP26()
+    {
+        var cabac = new CabacContext(15, 0, false, false, 26);
+        Assert.Equal(9, cabac.PStateIdx);
+        Assert.False(cabac.ValMps);
+    }
+
+    [Fact]
+    public void Test_CtxIdx280_CII0_Bypass0_I0_QP46()
+    {
+        var cabac = new CabacContext(280, 0, false, false, 46);
+        Assert.Equal(16, cabac.PStateIdx);
+        Assert.False(cabac.ValMps);
+    }
+}
