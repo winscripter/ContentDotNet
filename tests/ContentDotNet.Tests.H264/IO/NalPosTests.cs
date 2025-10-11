9a98655b6422c2569584ae11ff971bfabdb1560d
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-10-11 19:33:32 +0400

    New test

---
+++ tests/ContentDotNet.Tests.H264/IO/NalPosTests.cs
@@ -0,0 +1,37 @@
+﻿namespace ContentDotNet.Tests.H264.IO
+{
+    using ContentDotNet.BitStream;
+    using ContentDotNet.Extensions.Video.H264;
+
+    public class NalPosTests
+    {
+        [Fact]
+        public void Detect_Nal_Start()
+        {
+            var bytes = new byte[]
+            {
+                // Random bytes that should be skipped
+                0xA5, 0x89, 0x73, 0x63,
+
+                // NAL start
+                0x00, 0x00, 0x00, 0x01,
+
+                // Filler bytes
+                0x55, 0x00, 0x00,
+            };
+
+            var ms = new MemoryStream(bytes)
+            {
+                Position = 0
+            };
+            var bsr = new BitStreamReader(ms);
+
+            var dcd = new H264Service().CreateDecoder(bsr);
+
+            if (!dcd.SkipToNalStart())
+                Assert.Fail("Could not find NAL start!!!");
+
+            Assert.Equal(Array.IndexOf(bytes, 0x55), (int)ms.Position);
+        }
+    }
+}
