682372258378c54ba93cbdabbaebdd69221fa4b3
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-10-11 19:39:04 +0400

    New test

--- tests/ContentDotNet.Tests.H264/IO/NalPosTests.cs
+++ tests/ContentDotNet.Tests.H264/IO/NalPosTests.cs
@@ -33,5 +33,34 @@
 
             Assert.Equal(8, (int)ms.Position);
         }
+
+        [Fact]
+        public void Detect_Nal_Start_Without_Any()
+        {
+            var bytes = new byte[]
+            {
+                // Random bytes that should be skipped
+                0xA5, 0x89, 0x73, 0x63,
+
+                // Bogus
+                0x08, 0x42, 0x76, 0x94,
+            };
+
+            var ms = new MemoryStream(bytes)
+            {
+                Position = 0
+            };
+            var bsr = new BitStreamReader(ms);
+
+            var dcd = new H264Service().CreateDecoder(bsr);
+
+            // Read one byte to keep track of position
+            Assert.Equal(0xA5u, dcd.BitStreamReader.ReadByte());
+
+            // Skip and ensure
+            Assert.False(dcd.SkipToNalStart());
+
+            Assert.Equal(1, (int)ms.Position);
+        }
     }
 }
