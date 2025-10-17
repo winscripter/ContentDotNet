8beb60fad0111cd4facb8f65791f48c4df42d1ba
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-10-17 18:18:24 +0400

    New implementation. Wish me luck.

--- src/Codecs/Video/ContentDotNet.Extensions.Video.H264/Utilities/NalHelpers.cs
+++ src/Codecs/Video/ContentDotNet.Extensions.Video.H264/Utilities/NalHelpers.cs
@@ -82,5 +82,79 @@
             reader.BaseStream.Position = originalPosition;
             return false;
         }
+
+        /// <summary>
+        ///   Returns the length of the current NAL unit, assuming that the position of the NAL unit is within
+        ///   the NAL data boundary, which can include the NAL Unit Header or RBSP bytes. In the following example:
+        ///   <code>
+        ///     1A B5 00 00 01 50 74 38 25 09 00 00 00 01 52 56
+        ///                    ^
+        ///                    |
+        ///                    Position is here
+        ///   </code>
+        ///   <para>That will return 5.</para>
+        ///   <para>In the following example:</para>
+        ///   <code>
+        ///     1A B5 00 00 01 50 74 38 25 09 00 00 00 01 52 56
+        ///                                               ^
+        ///                                               |
+        ///                                               Position is here
+        ///   </code>
+        ///   <para>That returns 2.</para>
+        ///   <para>This method works for both NAL units with a 3 byte start code <c>00 00 01</c> and 4-byte start code <c>00 00 00 01</c>.</para>
+        ///   <para>This method will never throw.</para>
+        ///   <para>This method retains the location in the specified <paramref name="reader"/> when returning.</para>
+        /// </summary>
+        /// <param name="reader">
+        ///   Input bit-stream reader.
+        /// </param>
+        /// <returns>
+        ///   The length of the NAL unit, in bytes.
+        /// </returns>
+        public static long GetNalUnitLength(BitStreamReader reader)
+        {
+            // Align to byte boundary
+            while (reader.GetState().BitPosition != 0)
+                _ = reader.ReadBit();
+
+            var stream = reader.BaseStream;
+            long startPosition = stream.Position;
+
+            int b1, b2 = -1, b3 = -1, b4 = -1;
+            long currentPosition = startPosition;
+
+            while (true)
+            {
+                int byteRead = stream.ReadByte();
+                if (byteRead == -1)
+                    break;
+
+                currentPosition++;
+
+                b1 = b2;
+                b2 = b3;
+                b3 = b4;
+                b4 = byteRead;
+
+                // Check for 4-byte start code
+                if (b1 == 0x00 && b2 == 0x00 && b3 == 0x00 && b4 == 0x01)
+                {
+                    stream.Position = startPosition;
+                    return currentPosition - startPosition - 4;
+                }
+
+                // Check for 3-byte start code
+                if (b2 == 0x00 && b3 == 0x00 && b4 == 0x01)
+                {
+                    stream.Position = startPosition;
+                    return currentPosition - startPosition - 3;
+                }
+            }
+
+            // No further start code found; return until end of stream
+            long endPosition = stream.Position;
+            stream.Position = startPosition;
+            return endPosition - startPosition;
+        }
     }
 }
