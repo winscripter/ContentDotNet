d5b8ef7b661783c81e474289bd1eb6d187299019
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-10-21 22:14:01 +0400

    First SDP test

---
+++ tests/ContentDotNet.Tests.Sdp/SdpTests.cs
@@ -0,0 +1,16 @@
+﻿namespace ContentDotNet.Tests.Sdp
+{
+    using ContentDotNet.Protocols.Sdp.Lines;
+
+    public class SdpTests
+    {
+        [Fact]
+        public void TestVLine()
+        {
+            string source = "v=0";
+            var sdpV = new SdpVersionLine(source);
+            Assert.True(sdpV.TryGetVersion(out int version));
+            Assert.Equal(0, version);
+        }
+    }
+}
