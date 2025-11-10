85262391c2aa0363bf92525910991dc7885130a5
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-11-10 17:31:48 +0400

    Merged even more things

--- src/ContentDotNet.Video/Codecs/H264/Components/H264MacroblockChromaSizes.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ContentDotNet.Video.Codecs.H264.Components
-{
-    /// <summary>
-    ///   Chroma macroblock sizes
-    /// </summary>
-    public record struct H264MacroblockChromaSizes(int MbWidthC, int MbHeightC);
-}
