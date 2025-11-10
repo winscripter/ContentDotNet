348d352db9e5cbb7e080189fc04163c950efc469
VisualStudio2022User <75rxthebestguy@gmail.com>
2025-11-10 16:52:23 +0400

    Add multiplexer abstraction

---
+++ src/ContentDotNet.Video/Formats/Mp4/Mp4DataMultiplexer.cs
@@ -0,0 +1,16 @@
+﻿namespace ContentDotNet.Video.Formats.Mp4
+{
+    using ContentDotNet.Video.Formats.Mp4.Boxes;
+
+    /// <summary>
+    ///   Abstracts multiplexing MP4 data
+    /// </summary>
+    public abstract class Mp4DataMultiplexer
+    {
+        /// <summary>
+        ///   Creates the MP4 track.
+        /// </summary>
+        /// <returns>The track.</returns>
+        public abstract TrackBox CreateTrack();
+    }
+}
